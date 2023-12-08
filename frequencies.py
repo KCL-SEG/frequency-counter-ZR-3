2583a376a2a8d732afedab4a426071955a3e551e
ZR-3 <115020991+ZR-3@users.noreply.github.com>
2023-12-08 00:07:06 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if not isinstance(item, str):
+            item_string = str(item)
+        else:
+            item_string = item
+
+        if item_string in frequencies:
+            frequencies[item_string] += 1
+        else:
+            frequencies[item_string] = 1
     return frequencies
